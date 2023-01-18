539e8ed8b98ae4e25c498731a2309252c6be6b89
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-18 12:54:05 -0800

    Added navbar

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 import Image from "next/image";
 
 const styles = {
-    header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`
+    header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
+    headerwrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+    nav: `flex justify-center items-center gap-[20px]`,
+    navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
+    badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`, 
 };
 
 const Header = () => {
@@ -15,8 +19,43 @@ const Header = () => {
       />
 
       <div className={styles.headerwrapper}>
-        <div className={styles.nav}>
+        <nav className={styles.nav}>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Cryptocurrencies</div>
+                <div className={styles.badge}/>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Exchanges</div>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>NFT</div>
+                <div className={styles.badge}/>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Cryptown</div>
+                <div className={styles.badge}/>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Portfolio</div>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Watchlist</div>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Products</div>
+                <div className={styles.badge}/>
+            </div>
+            <div className={styles.navItem}>
+                <div className={styles.navLink}>Learn</div>
+            </div>
+        </nav>
 
+        <div className='flex items-center'>
+            { /* <ConnectButton /> */ }
+            <div className={styles.inputContainer}>
+                {/* <Search /> */}
+                <input className={styles.input} placeholder = 'Search'/>
+            </div>
         </div>
       </div>
     </div>
