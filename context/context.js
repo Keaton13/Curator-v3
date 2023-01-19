0bbff0d22ed7f0b84f133cadea95b329ea0331de
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-19 13:33:34 -0800

    Setting up context

---
+++ context/context.js
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CoinMarketContext = createContext()
+
+export const CoinMarketProvider = ({children}) => {
+
+    const getTopTenCoins = async () => {
+        try {
+            const res = await fetch('/api/geTopTen')
+            const data = await res.json();
+            return data.data.data
+        } catch(e) {
+            console.log(e.message)
+        }
+    }
+
+    return (
+        <CoinMarketContext.Provider
+        value = {{
+            getTopTenCoins
+        }}
+        >
+            {children}
+        </CoinMarketContext.Provider>
+    )
+}
