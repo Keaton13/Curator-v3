e8e67f3b5d77352af6f4ddddda90dd2c2098c207
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-23 18:11:13 -0800

    Created info.js and CoinNameRow

---
+++ pages/currencies/info.js
@@ -0,0 +1,90 @@
+import Header from "../../components/Header";
+import Solana from "../../assets/solana.png";
+import Usd from "../../assets/svg/usd";
+import { useEffect, useState } from "react";
+
+const styles = {
+  activeTab: `p-1 px-2 mr-2 rounded-lg bg-[#171924]`,
+  tabItem: `px-2`,
+  tabContainer: `flex items-center p-2 rounded-xl bg-[#222531] border-grey-500/10 text-sm`,
+  info: `min-h-screen`,
+  main: `text-white mx-auto max-w-screen-2xl`,
+  flexStart: `flex items-start`,
+  flexBetween: `flex justify-between`,
+  flexBetweenCenter: `flex justify-between items-center`,
+  tabContainerWrapper: `p-0 pl-0 pr-0 w-2/3`,
+  flexCenter: `flex items-center`,
+};
+
+const currencies = () => {
+  const [coinName, setCoinName] = useState("");
+  const [coinSymbol, setCoinSymbol] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    getURLData();
+  }, []);
+
+  const getURLData = async () => {
+    const queryString = window.Location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    setCoinName(urlParams.get("coin"));
+    setPrice(urlParams.get("price").toLocalString());
+    setCoinSymbol(urlParams.get("symbol"));
+  };
+
+  return (
+    <div className={styles.info}>
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.flexStart}>
+          <div className={styles.tabContainerWrapper}>
+            <div className={styles.flexBetween}>
+              <div className={styles.tabContiner}>
+                <p className={styles.activeTab}>Price</p>
+                <p className={styles.tabItem}>Market Cap</p>
+                <p className={styles.tabItem}>Trading View</p>
+                <p className={styles.tabItem}>History</p>
+              </div>
+              <div className={styles.tabContainer}>
+                <p className={styles.activeTab}>1D</p>
+                <p className={styles.tabItem}>2D</p>
+                <p className={styles.tabItem}>1M</p>
+                <p className={styles.tabItem}>3M</p>
+                <p className={styles.activeTab}>1Y</p>
+                <p className={styles.tabItem}>YTD</p>
+                <p className={styles.tabItem}>ALL</p>
+                <p className={styles.tabItem}>LOG</p>
+              </div>
+            </div>
+            <br />
+            { /* <Graph/> */}
+            <br />
+            <div className={styles.flexBetweenContianer}>
+                <div className='flex'>
+                    <div className={styles.flexContainer}>
+                        <input className='outline-none' type='checkbox'/> &nbsp; USD
+                    </div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div className={styles.flexCenter}>
+                        <input type='checkbox' /> &nbsp; BTC
+                    </div>
+                </div>
+
+                <p>
+                    Want more data?{''}
+                    <span className='text-[#6188FF]'>Check out our API!</span>
+                </p>
+            </div>
+            <br />
+            <br />
+            {/*  <Chat/> */}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Currencies;
