7af21f9ac7713cc1b0917db2326396eaa6086373
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-26 10:29:27 -0800

    Created CoinNameRow and modified CMCTableRow

--- components/cmc-table/CMCTableRow.js
+++ components/cmc-table/CMCTableRow.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import More from "../../assets/svg/more";
 import Star from "../../assets/svg/star";
 import Rate from "./Rate";
-import CoinNameRow from "./CoinNameRow";
+import CoinNameRow from "../CoinNameRow";
 
 const styles = {
   tableRow: `text-white border-b border-grey-800 text-[0.93rem]`,
