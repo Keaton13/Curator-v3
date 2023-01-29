064cd24c1c63dc39b28f05a01ba9b0787ac45c3a
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-28 22:37:11 -0800

    Working on coinDetails front end

--- components/cmc-table/Rate.js
+++ components/cmc-table/Rate.js
@@ -8,21 +8,19 @@ const styles = {
   green: `ml-2 text-[#17C784]`,
 };
 
-const Rate = ({rate, symbol }) => {
+const Rate = ({ rate, symbol }) => {
   rate = parseFloat(rate).toFixed(2);
   return (
     <div className={styles.rate}>
+      {symbol ? <small className="text-gray-400">24h</small> : <></>}&nbsp;&nbsp;
       {rate >= 0 ? (
         <ChevronUp fill="#17C784" />
       ) : (
         <ChevronDown fill="#EA3943" />
       )}
-      <p className={rate >= 0 ? styles.green : styles.red}>{rate + "%"}&nbsp;&nbsp;</p> 
-      {symbol ? (
-        <small>24h</small>
-      ) : (
-        <></>
-      )}
+      <p className={rate >= 0 ? styles.green : styles.red}>
+        {rate + "%"}
+      </p>
     </div>
   );
 };
