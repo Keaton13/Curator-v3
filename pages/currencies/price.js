5c81028ae2bd2c508d5c8923621461d7203860e3
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-28 21:21:32 -0800

    Fixed errors and got data rending properly

--- pages/currencies/price.js
+++ pages/currencies/price.js
@@ -1,26 +1,47 @@
-import { useState, useEffect } from "react"
-import CoinDetails from "../../components/coinDetails"
-import Header from "../../components/header"
+import { useState, useEffect, useContext } from "react";
+import { CoinMarketContext } from "../../context/context";
+import CoinDetails from "../../components/coinDetails";
+import Header from "../../components/header";
 
 const Price = () => {
-    const [id, setId] = useState('');
+  let { top100Coins, coinMetaData } = useContext(CoinMarketContext);
+  const [id, setId] = useState("");
+  const [targetCoin, setTargetCoin] = useState(null);
+  const [targetCoinMetaData, setTargetCoinMetaData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const getData = () => {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        setId(Number(urlParams.get('id')));
+  const getData = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    setId(Number(urlParams.get("id")));
+  };
+
+  useEffect(() => {
+    if (top100Coins && coinMetaData) {
+      setTargetCoin(top100Coins.find((object) => object.id === id));
+      setTargetCoinMetaData(coinMetaData.find((object) => object.id === id));
+      setLoading(false);
     }
+  }, [top100Coins, coinMetaData, id])
 
-    return (
+  return (
     <div>
-        <Header />
-            <CoinDetails id={id} />
+      <Header />
+      {top100Coins && coinMetaData ? (
+        <CoinDetails
+          id={id}
+          coinData={targetCoin}
+          coinMetaData={targetCoinMetaData}
+        />
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-    )
-}
+  );
+};
 
-export default Price
+export default Price;
