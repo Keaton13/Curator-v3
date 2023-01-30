0967b425d361fc40bf834eba1f0cbe1cb0a4a385
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-01-29 18:27:46 -0800

    Got trending cards and cmcTable showing real info

--- components/Trending.js
+++ components/Trending.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { CoinMarketContext } from "../context/context";
 import ReactSwitch from "react-switch";
 import fire from "../assets/fire.png";
-import btc from "../assets/btc.png";
-import usdt from "../assets/usdt.png";
 import gainers from "../assets/gainers.png";
 import recent from "../assets/recent.png";
 import Rate from "./cmc-table/Rate";
@@ -11,38 +10,46 @@ import TrendingCard from "./TrendingCard";
 const styles = {
   trendingWrapper: `mx-auto max-w-screen-2xl`,
   h1: `text-3xl text-white`,
-  flexCenter:  `flex items-center`,
+  flexCenter: `flex items-center`,
 };
 
-const trendingData = [
-    {
-        number:1,
-        symbol: "BTC",
-        name: "bitcoin",
-        icon: btc,
-        isIncrement: true,
-        rate: "2.34%"
-    },
-    {
-        number:2,
-        symbol: "USDT",
-        name: "Teather",
-        icon: usdt,
-        isIncrement: false,
-        rate: "9.14%"
-    },
-    {
-        number:3,
-        symbol: "BTC",
-        name: "bitcoin",
-        icon: btc,
-        isIncrement: true,
-        rate: "2.34%"
-    }
-]
 
 const Trending = () => {
+  let { top100Coins, coinMetaData } = useContext(CoinMarketContext);
   const [checked, setChecked] = useState(false);
+  const [sortedMatchingData24h, setSortedMatchingData24h] = useState(null);
+  const [sortedMatchingData7d, setSortedMatchingData7d] = useState(null);
+  const [sortedMatchingLast3, setSortedMatchingLast3] = useState(null);
+
+  useEffect(() => {
+    if (top100Coins && coinMetaData) {
+      const sortedMatchingMetaData7d = sortAndGetTop("percent_change_7d");
+      setSortedMatchingData7d(sortedMatchingMetaData7d);
+      const sortedMatchingMetaData24h = sortAndGetTop("percent_change_24h");
+      setSortedMatchingData24h(sortedMatchingMetaData24h);
+      const sortedMatchingLast3 = sortAndGetLast3();
+      setSortedMatchingLast3(sortedMatchingLast3);
+    }
+  }, [top100Coins, coinMetaData]);
+
+  function sortAndGetTop(field, top = 3) {
+    let sortedCoins = top100Coins.sort(
+      (a, b) => b.quote.USD[field] - a.quote.USD[field]
+    );
+    let coins = sortedCoins.slice(0, top);
+    let coinMap = new Map(coinMetaData.map((item) => [item.id, item]));
+    let coinsMapped = coins.map((coin) => coinMap.get(coin.id));
+    return {coins, coinsMapped}
+  }
+
+  function sortAndGetLast3() {
+    console.log(top100Coins)
+    let coins = top100Coins.slice(top100Coins.length - 3);
+    let coinMap = new Map(coinMetaData.map((item) => [item.id, item]));
+    let coinsMapped = coins.map((coin) => coinMap.get(coin.id));
+    return {coins, coinsMapped}
+  }
+
   return (
     <div className="text-white">
       <div className={styles.trendingWrapper}>
@@ -78,14 +85,27 @@ const Trending = () => {
         <br />
 
         <div className={styles.flexCenter}>
-          
-              <TrendingCard title = 'Trending' icon={fire} 
-              trendingData = {trendingData} />
-              <TrendingCard title = 'Biggest Gainers' icon={gainers} 
-              trendingData = {trendingData} />
-              <TrendingCard title = 'Recently Added' icon={recent} 
-              trendingData = {trendingData} />
-             
+          {top100Coins && coinMetaData ? (
+            <>
+              <TrendingCard
+                title="Trending"
+                icon={fire}
+                sortedMatchingMetaData = {sortedMatchingData7d}
+              />
+              <TrendingCard
+                title="Biggest Gainers"
+                icon={gainers}
+                sortedMatchingMetaData = {sortedMatchingData24h}
+              />
+              <TrendingCard
+                title="Recently Added"
+                icon={recent}
+                sortedMatchingMetaData = {sortedMatchingLast3}
+              />
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
