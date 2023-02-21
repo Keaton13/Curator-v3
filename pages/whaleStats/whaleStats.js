e1940cc3d49d9b1f479b20756185be946085ef32
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-02-20 17:32:26 -0800

    Created WhaleStats.js

---
+++ pages/whaleStats/whaleStats.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+const WhaleStats = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default WhaleStats
